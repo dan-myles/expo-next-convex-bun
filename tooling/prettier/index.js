12829474e1c3ed6dc2613ad0cec2c4194d90cce7
dan-myles <79137382+dan-myles@users.noreply.github.com>
2025-07-17 12:27:08 -0500

    fix: remove parser plugin for oxc

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -31,8 +31,6 @@ const config = {
     "^[../]",
     "^[./]",
   ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "4.4.0",
 }
 
 export default config
