a30bfcd62b1c1ec281b31a7fa26b42c9358378f7
dan-myles <79137382+dan-myles@users.noreply.github.com>
2025-07-20 17:20:20 -0500

    feat: remove external link

--- apps/native/components/EditScreenInfo.tsx
+++ apps/native/components/EditScreenInfo.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { StyleSheet } from "react-native"
 
-import Colors from "@/constants/Colors"
-import { ExternalLink } from "./ExternalLink"
 import { MonoText } from "./StyledText"
 import { Text, View } from "./Themed"
 
@@ -36,17 +34,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
         </Text>
       </View>
 
-      <View style={styles.helpContainer}>
-        <ExternalLink
-          style={styles.helpLink}
-          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet"
-        >
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making
-            changes
-          </Text>
-        </ExternalLink>
-      </View>
+      <View style={styles.helpContainer}></View>
     </View>
   )
 }
