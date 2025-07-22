bc588118bce0fd4abb01fefeda6b45c20aac7f3d
dan-myles <79137382+dan-myles@users.noreply.github.com>
2025-07-22 09:38:23 -0500

    feat: expo app react 19 pinned version

---
+++ apps/native/src/app/_layout.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react"
+import { useFonts } from "expo-font"
+import { Stack } from "expo-router"
+import * as SplashScreen from "expo-splash-screen"
+import FontAwesome from "@expo/vector-icons/FontAwesome"
+
+import "react-native-reanimated"
+
+export { ErrorBoundary } from "expo-router"
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  })
+
+  useEffect(() => {
+    if (error) throw error
+  }, [error])
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
+
+  return <Stack />
+}
